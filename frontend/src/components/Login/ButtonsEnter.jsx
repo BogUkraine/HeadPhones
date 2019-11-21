6924de16c733912735e1e7b1eaba37568eeefbe9
Bohdan Lysov <BogUkraine@gmail.com>
2019-11-21 20:28:47 +0200

    working login and url change

--- frontend/src/components/Login/ButtonsEnter.jsx
+++ frontend/src/components/Login/ButtonsEnter.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { NavLink } from 'react-router-dom';
 
-const ButtonsEnter = (({login, pass, userInfo}) => {
+/*const ButtonsEnter = (({login, pass, userInfo}) => {
 
     let isLogin = false;
 
@@ -32,7 +32,51 @@ const ButtonsEnter = (({login, pass, userInfo}) => {
         </div>
 	);
 })
+*/
 
+
+class ButtonsEnter extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+
+        let isLogin = false;
+
+        for(let i = 0; i < this.props.userInfo.length; i++) {
+            if((this.props.login === this.props.userInfo[i].user_name) && (this.props.pass === this.props.userInfo[i].user_password)){
+                isLogin = true;
+                break;
+            }
+            else {
+                event.preventDefault();
+                alert("Wrong password or login");
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className="form__buttons">
+                <button className="form__buttons_create" value="Create account" id="toCreate">Create account</button>
+                <NavLink to="/home">
+                    <button
+                    className="form__buttons_enter"
+                    value="Enter"
+                    id="Login"
+                    onClick={this.handleClick}
+                    >
+                        Enter
+                    </button>
+                </NavLink>
+            </div>
+        );
+    }
+}
 
 
 export default ButtonsEnter;
