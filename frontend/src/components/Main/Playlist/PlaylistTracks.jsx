2b15ff3d5ffc9403cc4697a07e24ae6273047aa8
Bohdan Lysov <BogUkraine@gmail.com>
2019-11-21 08:51:13 +0200

    playlist css

--- frontend/src/components/Main/Playlist/PlaylistTracks.jsx
+++ frontend/src/components/Main/Playlist/PlaylistTracks.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 
 const PlaylistTracks = () => {
     return (
-        <div className="playlist-music-block">
-            <div className="playlist-music-block__track-name-wrapper">
-                <div className="playlist-music-block__img"></div>
-                <span className="playlist-music-block__track-name">track_name</span>
+        <div className="playlist__track">
+            <div className="playlist__track_title">
+                <div className="playlist__track_img"></div>
+                <span className="playlist__track_name">track_name</span>
             </div>
-            <div className="playlist-music-block__description">
-                <span className="playlist-music-block__singer">singer</span>&#9679;
-                <span className="playlist-music-block__album">album</span>&#9679;
-                <span className="playlist-music-block__year">2019</span>
+            <div className="playlist__track_description">
+                <span className="playlist__track_singer">singer</span>
+                <span className="playlist__track_album">album</span>
+                <span className="playlist__track_year">2019</span>
             </div>
-            <div className="playlist-music-block__time">
-                <span className="playlist-">1:01</span>/
-                <span className="playlist-">3:30</span>
+            <div className="playlist__track_time">
+                <span className="current_time">1:01</span>
+                <span className="whole_time">3:30</span>
             </div>
         </div>
     );
